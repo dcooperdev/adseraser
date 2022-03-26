eb116e1d7d3d9327b6cfca3f4d8a149763ce48db
David <dcooper.dev@gmail.com>
2022-03-26 20:39:34 -0300

    added code to hide ads

--- eraseAds.js
+++ eraseAds.js
@@ -1,7 +1,2 @@
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete' && tab.active) {
-  
-      console.log({ tabId, changeInfo, tab });
-  
-    }
-  })
+const elements = document.querySelector('[aria-label="Ads"]');
+elements.style.display = 'none';
